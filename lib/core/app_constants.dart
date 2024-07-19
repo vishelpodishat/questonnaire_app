dd4de4b4cd13cf3cd0e93bac89201d4db62d83da
vishelpodishat <alishsaideshov@gmail.com>
2024-07-20 04:10:47 +0500

    App Const

---
+++ lib/core/app_constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class AppConstants {
+  static const Color tabBarColor = Color(0xffF2F2F7);
+  static const Color selectedTabsColor = Color(0xff29B1DD);
+  static const Color unselectedTabsColor = Color(0xff27272E);
+}
